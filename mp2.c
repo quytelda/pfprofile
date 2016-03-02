bc66020ea48d6fadd8bd87164bd4a4edbbab3b0e
Quytelda Kahja <quytelda@tamalin.org>
2016-03-02 00:17:48 -0600

    Add new proc file entry.

--- mp2.c
+++ mp2.c
@@ -12,9 +12,12 @@ MODULE_AUTHOR("Quytelda Kahja");
 MODULE_DESCRIPTION("Rate Monotonic Scheduler");
 
 #define DEBUG 1
-#define PROC_DIR "mp2"
+#define PROC_DIR   "mp2"
+#define PROC_ENTRY "status"
 
-struct proc_dir_entry * proc_dir;
+static struct proc_dir_entry * proc_dir;
+static struct proc_dir_entry * proc_entry;
+static const struct file_operations proc_fops = {};
 
 int __init mp2_init(void)
 {
@@ -24,6 +27,7 @@ int __init mp2_init(void)
 
     // set up proc filesystem entries
     proc_dir = proc_mkdir(PROC_DIR, NULL);
+    proc_entry = proc_create(PROC_ENTRY, 0666, proc_dir, &proc_fops);
     return 0;
 }
 
@@ -32,6 +36,8 @@ void __exit mp2_exit(void)
 #ifdef DEBUG
     printk(KERN_INFO "Cleaning up MP2 module...");
 #endif
+    // clean up proc filesystem entry
+    remove_proc_entry(PROC_ENTRY, proc_dir);
     remove_proc_entry(PROC_DIR, NULL);
 }
 
