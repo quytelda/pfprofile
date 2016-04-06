a8b9099221d03113627fb5755c465433bcdb3876
Quytelda Kahja <quytelda@tamalin.org>
2016-04-06 02:14:47 -0400

    Add character device.

--- src/mp3.c
+++ src/mp3.c
@@ -7,9 +7,8 @@
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
-#include <linux/sched.h>
 #include <linux/mm.h>
-#include "mp3_given.h"
+#include <linux/cdev.h>
 #include "task.h"
 #include "dispatch.h"
 
@@ -20,6 +19,7 @@ MODULE_DESCRIPTION("Rate Monotonic Scheduler");
 #define DEBUG 1
 #define PROC_DIR   "mp3"
 #define PROC_ENTRY "status"
+#define DEV_NAME   "node"
 #define NUM_PAGES 128
 
 static ssize_t mp2_read(struct file * file, char __user * buf,
@@ -69,7 +69,21 @@ static ssize_t mp2_write(struct file * file, const char * buf,
     return length;
 }
 
-static void * buffer;
+static int device_open(struct inode * node, struct file * file)
+{
+    return 0; // do nothing
+}
+
+static int device_release(struct inode * node, struct file * file)
+{
+    return 0; // do nothing
+}
+
+static int device_mmap(struct file * file, struct vm_area_struct * area)
+{
+    return 0;
+}
+
 static struct proc_dir_entry * proc_dir;
 static struct proc_dir_entry * proc_entry;
 static const struct file_operations proc_fops =
@@ -77,6 +91,17 @@ static const struct file_operations proc_fops =
     .owner = THIS_MODULE,
     .read  = mp2_read,
     .write = mp2_write,
+};
+
+static void * buffer;
+static dev_t dev_num;
+static struct cdev * dev;
+static const struct file_operations dev_fops =
+{
+    .owner   = THIS_MODULE,
+    .open    = device_open,
+    .release = device_release,
+    .mmap    = device_mmap,
 };
 
 int __init mp2_init(void)
@@ -97,6 +122,25 @@ int __init mp2_init(void)
 	set_bit(PG_reserved, &vmalloc_to_page(addr)->flags);
     }
 
+    // setup character device
+    int err;
+
+    err = alloc_chrdev_region(&dev_num, 0, 0, DEV_NAME);
+    dev = cdev_alloc();
+    if(err || !dev)
+    {
+	printk(KERN_ERR "mp3: Unable to allocate character device.");
+	return 0;
+    }
+    
+    cdev_init(dev, &dev_fops);
+    err = cdev_add(dev, dev_num, 0);
+    if(err)
+    {
+	printk(KERN_ERR "mp3: Unable to register character device.");
+	return 0;
+    }
+
     // set up proc filesystem entries
     proc_dir = proc_mkdir(PROC_DIR, NULL);
     proc_entry = proc_create(PROC_ENTRY, 0666, proc_dir, &proc_fops);
@@ -112,6 +156,10 @@ void __exit mp2_exit(void)
     // clean up proc filesystem entry
     remove_proc_entry(PROC_ENTRY, proc_dir);
     remove_proc_entry(PROC_DIR, NULL);
+
+    // clean up character device
+    cdev_del(dev);
+    unregister_chrdev_region(dev_num, 0);
 
     // clean up shared user-space memory buffer
     void * addr;
